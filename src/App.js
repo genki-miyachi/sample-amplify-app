938e11ced0dd2a73079f5445eda9caf2a826b6b8
Genki Miyachi <genki.miyachi@techouse.jp>
2023-03-17 15:36:14 +0900

    List 4.12

--- src/App.js
+++ src/App.js
@@ -2,20 +2,39 @@ import { useState } from 'react';
 import './App.css';
 import { Header } from './ui-components';
 
+function Hello(props) {
+  return (
+    <div className={"alert alert-" + props.type}>
+      {props.message}
+    </div>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [msg, setMsg] = useState("")
+  const [msgs, setMsgs] = useState([]) 
+  
+  const onChange = (e) => {
+    setMsg(e.target.value)
+  }
+  
   const onClick = () => {
-    setCount(count + 1)
+    setMsgs([
+      "Hello, " + msg + "!",
+      "こんにちわ、" + msg + "さん。"
+    ])
   }
   
   return (
     <div className="py-4">
       <Header className="mb-4" />
       <p>※これ、UIコンポーネントを利用した表示です</p>
-      <div className="alert alert-primary">Count: {count}</div>
-      <button className="btn btn-primary" onClick={onClick}>
-        Click me!
-      </button>
+      <div className="mx-0 my-3 row">
+        <input type="text" className="form-control col" onChange={onChange}/>
+        <button className="btn btn-primary col-2" onClick={onClick}>Click</button>
+      </div>
+      <Hello message={msgs[0]} type="primary" />
+      <Hello message={msgs[1]} type="dark"/>
     </div>
   );
 }
