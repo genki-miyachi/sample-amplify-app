e06d1a8a4d3705d457ab66edb0d1f172e49c5463
Genki Miyachi <genki.miyachi@techouse.jp>
2023-03-17 15:03:46 +0900

    List 4.8

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import './App.css';
 import { Header } from './ui-components';
 
+function Hello(props) {
+  return (
+    <p className={"alert alert-" + props.type}>{props.message}</p>
+  )
+}
+
 function App() {
   return (
     <div className="py-4">
       <Header className="mb-4" />
       <p>※これ、UIコンポーネントを利用した表示です</p>
-      <Hello />
-      <Now />
+      <Hello message="サンプルのメッセージです。" type="primary" />
+      <Hello message="表示のタイプも変えられます" type="dark" />
     </div>
   );
 }
 
-function Hello() {
-  return (
-    <p className="border border-primary p-3 my-3">こんにちわ！</p>
-  )
-}
-
-function Now() {
-  return <p className="bg-secondary text-dark bg-opacity-25 p-3 my-3">現在は{new Date().getHours()}時です</p>
-}
 export default App;
