6cb54e1bf652db1bb5ae7077db35f5243fa9a6e0
Genki Miyachi <genki.miyachi@techouse.jp>
2023-03-17 15:26:03 +0900

    List 4.10

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { Header } from './ui-components';
 
-function onClick() {
-  alert("クリックした！")
-}
-
 function App() {
+  let val = 0;
+  const onClick = () => {
+    val += 1
+  }
+  
   return (
     <div className="py-4">
       <Header className="mb-4" />
       <p>※これ、UIコンポーネントを利用した表示です</p>
+      <div className="alert alert-primary">Count: {val}</div>
       <button className="btn btn-primary" onClick={onClick}>
         Click me!
       </button>
