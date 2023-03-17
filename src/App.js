a1f10cecda8629736d79e354b26de3d9841d98fb
Genki Miyachi <genki.miyachi@techouse.jp>
2023-03-17 15:00:37 +0900

    List 4.8

--- src/App.js
+++ src/App.js
@@ -6,8 +6,19 @@ function App() {
     <div className="py-4">
       <Header className="mb-4" />
       <p>※これ、UIコンポーネントを利用した表示です</p>
+      <Hello />
+      <Now />
     </div>
   );
 }
 
+function Hello() {
+  return (
+    <p className="border border-primary p-3 my-3">こんにちわ！</p>
+  )
+}
+
+function Now() {
+  return <p className="bg-secondary text-dark bg-opacity-25 p-3 my-3">現在は{new Date().getHours()}時です</p>
+}
 export default App;
